6af6fbf31eedfa341671a125e97be0ab4f956f8b
Daniel Albuschat <d.albuschat@gmail.com>
2018-01-01 20:03:05 +0100

    Frontend: Added load generator for the Auto Scale experiment

--- src/frontend/AutoScaling.elm
+++ src/frontend/AutoScaling.elm
@@ -1,10 +1,14 @@
 module AutoScaling exposing (Model, Msg, init, view, update)
-import Html exposing (Html, h1, text, table, tr, td)
+import Html exposing (Html, h1, text, table, tr, td, button, div)
+import Html.Events exposing (onClick)
+import Http
+import String.Format exposing (format1)
 
 -- MODEL
 
 type alias Model = {
-    x: Int
+    isRunning: Bool,
+    finishedRequests: Int
 }
 
 type alias Container c = {
@@ -15,11 +19,13 @@ type alias Container c = {
 -- MSG
 
 type Msg =
-    None
+      StartLoadGenerator
+    | StopLoadGenerator
+    | ContinueLoadGenerator (Result Http.Error String)
 
 -- FUNCTIONS
 
-init = Model 0
+init = Model False 0
 
 
 renderAutoScaling : Model -> { name: String, status: String, app: String, podIP: String } -> Html msg
@@ -33,11 +39,39 @@ view : (Msg -> msg) -> List { name: String, status: String, app: String, podIP: 
 view makeMsg podList model =
     [
         h1 [] [ text "Experiment 3: Auto-Scaling" ],
+        if (model.isRunning) then
+            div [] [
+                button [ onClick (makeMsg StopLoadGenerator)] [ text "Stop load generator" ],
+                text (String.Format.format1 "{1} requests finished" model.finishedRequests)
+            ]
+        else
+            button [ onClick (makeMsg StartLoadGenerator)] [ text "Start load generator" ],
         table [] (List.map (renderAutoScaling model) (List.filter (\n -> n.app == "cpuhog") podList))
+    ]
+
+makeLoadGeneratorRequest makeMsg = 
+        Http.send (\n -> (makeMsg (ContinueLoadGenerator n))) (Http.getString "http://192.168.178.80:84/")
+
+-- The load generator fires two requests at once, because a single request would only occupy two nodes
+makeLoadGeneratorRequests makeMsg = 
+    Cmd.batch [
+        makeLoadGeneratorRequest makeMsg,
+        makeLoadGeneratorRequest makeMsg
     ]
 
 update : (Msg -> msg) -> Msg -> Container c -> (Container c, Cmd msg)
 update makeMsg msg model =
-    (model, Cmd.none)
-
+    let
+        autoScaling = model.autoScaling
+    in
+        case msg of
+            StartLoadGenerator ->
+                ({ model | autoScaling = { autoScaling | isRunning = True } }, makeLoadGeneratorRequests makeMsg)
+            StopLoadGenerator ->
+                ({ model | autoScaling = { autoScaling | isRunning = False, finishedRequests = 0 } }, Cmd.none)
+            ContinueLoadGenerator _ ->
+                if (autoScaling.isRunning) then
+                    ({ model | autoScaling = { autoScaling | finishedRequests = autoScaling.finishedRequests + 1 } }, makeLoadGeneratorRequests makeMsg)
+                else
+                    (model, Cmd.none)
 
