1c26f6c9365945abd134ec41872045fa814ed7f1
Daniel Albuschat <d.albuschat@gmail.com>
2018-01-23 15:59:03 +0000

    Refactor build to run (hopefully) successfully on x86_64 CI

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Check for mandatory parameters: 
+if [ ! -n "${DOCKER_USERNAME}" ]; then
+    echo "Env var DOCKER_USERNAME must be set.";
+    exit 1
+fi
+
 # Find out architecture or read it from command line:
 ARCH=$1
 if [ -z "${ARCH}" ]; then
@@ -10,22 +16,33 @@ if [ -z "${ARCH}" ]; then
     fi
 fi
 
-echo "Building docker images for architecture ${ARCH}."
+# Set appropriate BASEIMG depending on $ARCH:
+case $RARCH in
+    arm32v7)
+            # We need these base images, because they contain qemu-armhf-satic
+            GOLANG_BASEIMG=resin/raspberrypi3-golang 
+            NGINX_BASEIMG=alexellis2/nginx-arm
+        ;;
+    amd64)
+            GOLANG_BASEIMG=golang
+            NGINX_BASEIMG=nginx
+        ;;
+    *)
+        echo "Unsupported ARCH ${ARCH}"
+        exit 1
+        ;;
+esac
 
-# Check for mandatory parameters: 
-if [ ! -n "${DOCKER_USERNAME}" ]; then
-    echo "Env var DOCKER_USERNAME must be set.";
-    exit 1
-fi
+echo "Building docker images for architecture ${ARCH}."
 
 # Now build the docker containers:
 echo "Building getip..."
-docker build -t "${DOCKER_USERNAME}/getip_${ARCH}" src/getip --build-arg "ARCH=${ARCH}"
+docker build -t "${DOCKER_USERNAME}/getip_${ARCH}" src/getip --build-arg "BASEIMG=${GOLANG_BASEIMG}"
 echo "Building healthcheck..."
-# docker build -t "${DOCKER_USERNAME}/healthcheck_${ARCH}" src/healthcheck --build-arg "ARCH=${ARCH}"
+docker build -t "${DOCKER_USERNAME}/healthcheck_${ARCH}" src/healthcheck --build-arg "BASEIMG=${GOLANG_BASEIMG}"
 echo "Building cpuhog..."
-# docker build -t "${DOCKER_USERNAME}/cpuhog_${ARCH}" src/cpuhog --build-arg "ARCH=${ARCH}"
+docker build -t "${DOCKER_USERNAME}/cpuhog_${ARCH}" src/cpuhog --build-arg "BASEIMG=${GOLANG_BASEIMG}"
 echo "Building frontend..."
-#docker build -t "${DOCKER_USERNAME}/frontend_${ARCH}" src/frontend --build-arg "ARCH=${ARCH}"
+ocker build -t "${DOCKER_USERNAME}/frontend_${ARCH}" src/frontend --build-arg "BASEIMG=${NGINX_BASEIMG}"
 echo "Build finished."
 
