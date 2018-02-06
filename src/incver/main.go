356ac39e717562b5b6d769d42e53244c6721f746
Daniel Albuschat <d.albuschat@gmail.com>
2018-02-06 14:03:51 +0000

    Finished implementing a multi-receiver, single-sender mechanism

--- src/incver/main.go
+++ src/incver/main.go
@@ -4,6 +4,7 @@ import (
     "os/exec";
     "bufio";
     "fmt";
+    "time";
     )
 
 func readInBackground (reader *bufio.Reader, channel chan string) {
@@ -22,8 +23,29 @@ type StreamCommandOptions struct {
     errFormat string
 }
 
-func streamCommand(options StreamCommandOptions, name string, arg ...string) chan string {
-    resultChan := make(chan string, 1)
+type ReceiverNotification struct {
+    finished bool
+    payload string
+}
+
+type RegisterNotification struct {
+    registerChan chan ReceiverNotification
+    unregisterChan chan ReceiverNotification
+}
+
+func removeFromSlice(input []chan ReceiverNotification, chanToBeRemoved chan ReceiverNotification) []chan ReceiverNotification {
+    var result []chan ReceiverNotification
+    for _, c := range input {
+        if c != chanToBeRemoved {
+            result = append(result, c)
+        }
+    }
+    return result
+}
+
+func streamCommand(options StreamCommandOptions, name string, arg ...string) chan RegisterNotification {
+    registerChan := make(chan RegisterNotification, 1)
+    var receiverChans []chan ReceiverNotification;
     go func() {
         cmd := exec.Command(name, arg...)
         cmdOutPipe, err := cmd.StdoutPipe()
@@ -45,33 +67,75 @@ func streamCommand(options StreamCommandOptions, name string, arg ...string) cha
             select {
                 case line, ok := <-outChan:
                     if ok {
-                        resultChan <- line
+                        for _, receiverChan := range receiverChans {
+                            notif := ReceiverNotification{false, line}
+                            receiverChan <- notif
+                        }
                     } else {
                         exitOut = true
                     }
                 case line, ok := <-errChan:
                     if ok {
-                        resultChan <- line
+                        for _, receiverChan := range receiverChans {
+                            notif := ReceiverNotification{false, line}
+                            receiverChan <- notif
+                        }
                     } else {
                         exitErr = true
                     }
+                case registerNotif, _ := <-registerChan:
+                    if registerNotif.registerChan != nil {
+                        receiverChans = append(receiverChans, registerNotif.registerChan)
+                    }
             }
         }
+        for _, receiverChan := range receiverChans {
+            receiverChan <- ReceiverNotification{true, ""}
+        }
+        receiverChans = []chan ReceiverNotification{}
+        close(registerChan)
+        fmt.Printf("Waiting for cmd\n")
         cmd.Wait()
-        close(resultChan)
+        fmt.Printf("cmd finished\n")
     } ()
-    return resultChan
+    return registerChan
 }
 
-func main() {
-    channel := streamCommand(StreamCommandOptions{}, "sh", "-c", "./test.sh")
+func readOutput(prefix string, registerChan chan RegisterNotification) {
+    receiveChan := make(chan ReceiverNotification, 1)
+    registerChan <- RegisterNotification{receiveChan,nil}
     for {
-        line, ok := <-channel
+        notif, ok := <-receiveChan
         if ok {
-            fmt.Printf(line)
+            if notif.finished {
+                fmt.Printf("Closing %s\n", prefix)
+                close(receiveChan)
+                break
+            } else {
+                fmt.Printf("%s: %s", prefix, notif.payload)
+            }
         } else {
             break
         }
     }
+    fmt.Printf("Done with %s\n", prefix)
+}
+
+func main() {
+    registerChan := streamCommand(StreamCommandOptions{}, "sh", "-c", "./test.sh")
+    finishChan := make(chan ReceiverNotification, 1)
+    registerChan <- RegisterNotification {finishChan, nil}
+    go readOutput("A", registerChan)
+    go readOutput("B", registerChan)
+    go readOutput("C", registerChan)
+    duration, _ := time.ParseDuration("2s")
+    time.Sleep(duration)
+    for {
+        notif, ok := <-finishChan
+        if notif.finished || !ok {
+            break
+        }
+    }
+
 }
 
