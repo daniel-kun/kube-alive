af46818d2a250d6cf52f8ebeca4feca4f664a011
Daniel Albuschat <d.albuschat@gmail.com>
2018-02-17 07:05:57 +0000

    Small fixes

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ -z "`kubectl version`" ]; then
     exit 1
 fi
 
-export KUBEALIVE_PUBLICIP=`kubectl config view --minify=true | grep server | sed 's/.*http:\/\/\(.*\)/\1/'`
+export KUBEALIVE_PUBLICIP=`kubectl config view --minify=true | grep "server: http" | sed 's/.*https*:\/\/\(.*\):[0-9]*$/\1/'`
 echo "Using ${KUBEALIVE_PUBLICIP} as the exposed IP to access kube-alive."
 
 ARCHSUFFIX=
