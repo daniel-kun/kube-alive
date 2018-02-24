6703c14ef2acefd587c84a44b4a7fbc2adbe486e
Daniel Albuschat <d.albuschat@gmail.com>
2018-02-24 16:49:06 +0100

    Frontend: Optical fine-tuning for Experiment #1 LoadBalancing

--- src/frontend/src/LoadBalancing.elm
+++ src/frontend/src/LoadBalancing.elm
@@ -1,4 +1,5 @@
 module LoadBalancing exposing (Model, Msg(ExecLoadBalanceTest, ReceiveLoadBalanceResponse), init, update, view)
+
 import Base exposing (PodInfo, CommonModel)
 import String.Format exposing (format1)
 import Html exposing (..)
@@ -13,51 +14,68 @@ import Material.Color as Color
 import Material.Button as Button
 import Material.List as Lists
 import Material.Badge as Badge
+import Material.Typography as Typo
+
 
 -- MODEL
 
+
 type alias Model =
-    { responses: List String,
-      originHost: String,
-      mdl : Material.Model
+    { responses : List String
+    , originHost : String
+    , mdl : Material.Model
     }
+
+
 
 -- MSG
 
-type Msg =
-      ExecLoadBalanceTest 
+
+type Msg
+    = ExecLoadBalanceTest
     | ReceiveLoadBalanceResponse (Result Http.Error String)
     | Mdl (Material.Msg Msg)
 
+
+
 -- FUNCTIONS
 
-init originHost = Model [] originHost Material.model
+
+init originHost =
+    Model [] originHost Material.model
 
 
-renderLoadBalancing : Model -> PodInfo  -> Html msg
+renderLoadBalancing : Model -> PodInfo -> Html msg
 renderLoadBalancing loadBalancing pod =
-    Lists.li [] [
-        Lists.content [] [ text pod.name, Lists.subtitle [] [ text pod.status ] ],
-        Lists.content2 [] [ Options.span [ Badge.add (toString (List.Extra.count (\n -> n == pod.podIP) loadBalancing.responses)) ] [ text "Responses" ] ]
-    ]
+    Lists.li [ Lists.withSubtitle ]
+        [ Lists.content [] [ text pod.name, Lists.subtitle [] [ text pod.status ] ]
+        , Lists.content2 [] [ Options.span [ Badge.add (toString (List.Extra.count (\n -> n == pod.podIP) loadBalancing.responses)) ] [ text "Responses" ] ]
+        ]
+
 
 view : CommonModel -> Model -> List (Html Msg)
 view commonModel loadBalancing =
-    [
-        Options.styled h1 [Color.text Color.primary] [ text "Experiment 1: Load-Balancing" ],
-        Button.render Mdl [0] commonModel.mdl [ Button.raised, Button.colored, Button.ripple, Options.onClick ExecLoadBalanceTest ] [text "Make 50 requests to getip"],
-        Lists.ul [] (List.map (renderLoadBalancing loadBalancing) (List.filter (\n -> n.app == "getip") commonModel.podList))
+    [ Options.styled h1 [ Color.text Color.primary ] [ text "Experiment 1: Load-Balancing" ]
+    , Options.styled p
+        [ Typo.body1 ]
+        [ text "In this experiment, you can make requests to a load-balanced service backed by multiple, stateless instances. Press the button below and observe how the requests are balanced between the Pods." ]
+    , Button.render Mdl [ 0 ] commonModel.mdl [ Button.raised, Button.colored, Button.ripple, Options.onClick ExecLoadBalanceTest ] [ text "Make 50 requests" ]
+    , Lists.ul [] (List.map (renderLoadBalancing loadBalancing) (List.filter (\n -> n.app == "getip") commonModel.podList))
     ]
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ExecLoadBalanceTest ->
-            (model, Cmd.batch (List.repeat 50 (Http.send ReceiveLoadBalanceResponse (Http.getString (format1 "http://{1}/getip" model.originHost)))))
+            ( model, Cmd.batch (List.repeat 50 (Http.send ReceiveLoadBalanceResponse (Http.getString (format1 "http://{1}/getip" model.originHost)))) )
+
         ReceiveLoadBalanceResponse (Ok response) ->
-            ({ model | responses = response :: model.responses }, Cmd.none)
+            ( { model | responses = response :: model.responses }, Cmd.none )
+
         ReceiveLoadBalanceResponse (Err _) ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
+
         Mdl msg_ ->
             Material.update Mdl msg_ model
 
