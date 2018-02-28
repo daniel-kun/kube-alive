826cc4c03821aac59ff7ba6c4502d0460ba8bf9f
Daniel Albuschat <d.albuschat@gmail.com>
2018-02-28 21:47:51 +0100

    New service for new experiment: 'storer' stores values in a local file

---
+++ src/storer/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "net/http"
+import "io/ioutil"
+import "github.com/gorilla/mux"
+
+
+
+func main() {
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/retrieve", func(w http.ResponseWriter, r *http.Request) {
+        text, err := ioutil.ReadFile("text")
+        if err != nil {
+            fmt.Fprintf(w, "")
+        } else {
+            fmt.Fprintf(w, string(text))
+        }
+	})
+	router.HandleFunc("/store/{text}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        text := vars["text"]
+        ioutil.WriteFile("text", []byte(text), 0600)
+	})
+	fmt.Printf("'storer' server starting, listening to 8080 on all interfaces.\n")
+	http.ListenAndServe(":8080", router)
+}
+
